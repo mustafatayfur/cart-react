a3f206a65dc96b5584c43e4c906eb678a68f7c2a
mustafa <mltm0204@gmail.com>
2021-12-29 13:42:30 +0300

    fourth commit

--- src/reducer.js
+++ src/reducer.js
@@ -14,7 +14,15 @@ const reducer = (state, action)=>{
         })
         return {...state, cart: tempCart}
     }
-  
+    if(action.type === 'DECREASE'){
+        let tempCart = state.cart.map((cartItem)=> {
+            if(cartItem.id === action.payload){
+                return{...cartItem, amount: cartItem.amount - 1}
+            }
+            return cartItem
+        })
+        return {...state, cart: tempCart}
+    }
  
     return state
 }
