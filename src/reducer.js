d5652bc5e454816620c88cae50befe43b041bab5
mustafa <mltm0204@gmail.com>
2021-12-29 15:36:26 +0300

    ...

--- src/reducer.js
+++ src/reducer.js
@@ -30,7 +30,7 @@ const reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
   if (action.type === "GET_TOTALS") {
-    const { total, amount } = state.cart.reduce(
+    let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
         const { price, amount } = cartItem;
         const itemTotal = price * amount;
@@ -42,9 +42,35 @@ const reducer = (state, action) => {
         total: 0,
         amount: 0,
       }
-    );
+    )
+    total = parseFloat(total.toFixed(2))
+    
     return { ...state, total, amount };
   }
-  return state;
+
+  if(action.type === 'LOADING'){
+      return {...state, loading: true}
+  }
+  if(action.type === 'DISPLAY_ITEMS'){
+      return {...state, cart: action.payload, loading: false}
+  }
+  if(action.type=== 'TOGGLE_AMOUNT'){
+      let tempCart = state.cart.map((cartItem)=>{
+          if(cartItem.id === action.payload.id){
+            if(action.payload=== 'inc'){
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            if(action.payload.type=== 'dec'){
+                return{...cartItem, amount: cartItem.amount - 1}
+            }
+          }
+          return cartItem
+      })
+      .filter((cartItem)=> cartItem.amount !== 0)
+
+    return{...state, cart:tempCart}
+  }
+
+  throw new Error('no matching action type')
 };
 export default reducer;
